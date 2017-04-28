7775acef01d39eaa90c73a1022b02cb576af9030
Arkadiusz Surma <arkadiusz@linsafe.co.uk>
2017-04-28 21:55:22 +0100

    InsertionSort

---
+++ InsertionSort.h
@@ -0,0 +1,20 @@
+#include <vector>
+
+using namespace std;
+
+void InsertionSort(vector<int> E) {
+    int i = 0, j = 0, swapAmount = 0, size = (int) E.size();
+
+    for (i = 0; i < size; ++i) {
+
+        printf("Outer loop iteration num: %d \n", i);
+        j = i;
+        while ((j > 0) && (E[j - 1] > E[j])) {
+            swap(E[j - 1], E[j]);
+            j--;
+            swapAmount++;
+
+            printf("    Swap amount: %d\n", swapAmount);
+        }
+    }
+}
