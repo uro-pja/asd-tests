20ed9ff04c01c582484d5a71caf9136c1de95ea5
Arkadiusz Surma <arkadiusz@linsafe.co.uk>
2017-05-13 20:02:06 +0100

    Fix DFS

--- main.cpp
+++ main.cpp
@@ -4,25 +4,22 @@
 
 int main() {
 
-//    ExpressionValue("((0+1)*((6*(7+8))*3))");
+    ExpressionValue("((1+(4+9))+((2+4)+9))");
 
-    std::vector<std::vector<int>> in = {
-            {1, 3, 4, 7},
-            {0, 2, 3, 4, 5, 6},
-            {1, 7},
-            {0, 1, 5, 6, 7},
-            {0, 1, 5},
-            {1, 3, 4, 6},
-            {1, 3, 5},
-            {0, 2, 3}
-    };
-
-    Graph *graph = new Graph(in);
-    Vertex *vertex = graph->get(7);
-    std::vector<Vertex *> L = DFS(graph, vertex);
-    for (Vertex *elem: L) {
-
-    }
+//    std::vector<std::vector<int>> in = {
+//            {1,2,4,5,6,7},
+//            {0,3,4,5},
+//            {0,3},
+//            {1,2,4,7},
+//            {0,1,3,5},
+//            {0,1,4},
+//            {0,7},
+//            {0,3,6}
+//    };
+//
+//    Graph *graph = new Graph(in);
+//    Vertex *vertex = graph->get(4);
+//    std::vector<Vertex *> L = DFS(graph, vertex);
 
     return 0;
 }
